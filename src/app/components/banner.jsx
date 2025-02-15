a488fd8b16982c742ec02ab099bef0902484e6b7
adnanahid <sikuahamed@gmail.com>
2025-02-15 18:37:36 +0600

    sllider going on

--- src/app/components/banner.jsx
+++ src/app/components/banner.jsx
@@ -1,39 +1,29 @@
-"use client";
-import Image from "next/image";
-import React, { useState, useEffect } from "react";
+"use client"
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default function Banner() {
-  // State to keep track of the current image
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
 
-  // Array containing the images to display
-  const images = [
-    "/assets/images/banner/1.jpg",
-    "/assets/images/banner/2.jpg",
-    "/assets/images/banner/3.jpg",
-    "/assets/images/banner/4.jpg",
-    "/assets/images/banner/5.jpg",
-  ];
+// import required modules
+import { Navigation } from "swiper/modules";
 
-  // Function to change the image every 5 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 2000); // Change every 5 seconds
-
-    // Clean up interval on component unmount
-    return () => clearInterval(interval);
-  }, [images.length]);
-
+export default function App() {
   return (
-    <div className="h-[calc(100vh-64px)] relative">
-      <Image
-        src={images[currentImageIndex]} // Display the current image
-        layout="fill"
-        objectFit="cover"
-        objectPosition="top"
-        alt="Banner Image"
-      />
-    </div>
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
   );
 }
